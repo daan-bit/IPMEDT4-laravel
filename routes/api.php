a16e55d1572c9881c2ac7cd00c4b1b51c54304f6
prgnl <alexfilinsky@gmail.com>
2021-05-26 17:18:21 +0200

    JSON Webtokens werken succesvol en we krijgen een token terug via api in react

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+
+/*voor inloggen - alex*/
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
