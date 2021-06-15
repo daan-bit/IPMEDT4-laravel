9a0d5eee392eee1c385299b7bb0cb062875126ac
prgnl <alexfilinsky@gmail.com>
2021-06-15 11:30:09 +0200

    Update van de antwoordenseeder..

--- database/seeders/AntwoordenTableSeeder.php
+++ database/seeders/AntwoordenTableSeeder.php
@@ -15,84 +15,182 @@ class AntwoordenTableSeeder extends Seeder
     public function run()
     {
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '4',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '4',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '4',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '5',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '4',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '4',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '3',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '2',
         ]);
         DB::table('antwoorden')->insert([
-            'vraag_id' => 1,
+            'vraag_id' => 2,
             'antwoord' => '1',
+        ]);
+
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de vraag',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super slechte beantwoording van de vraag',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super goede beantwoording van de vraag',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de vraag 2',
+        ]);
+        
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de vraag 3',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test 3',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test ',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de vraag1',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
+        ]);
+        DB::table('antwoorden')->insert([
+            'vraag_id' => 1,
+            'antwoord' => 'Dit is een super leuke beantwoording van de test',
         ]);
 
         //
